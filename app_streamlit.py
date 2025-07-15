dd3a4461ac45d668bb38a0c244f1986b7c201c05
Anirudh Talluri <168444331+anitalluri00@users.noreply.github.com>
2025-07-15 13:25:51 +0530

    Update app_streamlit.py

--- app_streamlit.py
+++ app_streamlit.py
@@ -2,25 +2,19 @@ import numpy as np
 import pickle
 import pandas as pd
 import streamlit as st 
-
 from PIL import Image
-
 
 pickle_in = open("house_model.pkl","rb")
 classifier=pickle.load(pickle_in)
 
-
 def welcome():
     return "Welcome All"
-
 
 def predict_note_authentication(MedInc,HouseAge,Population,AveOccup):
    
     prediction=classifier.predict([[MedInc,HouseAge,Population,AveOccup]])
     print(prediction)
     return prediction
-
-
 
 def main():
     st.title("House Prediction: Boston City")
